de1e138760e90e551e85b01b2b6d95aee81d57a5
tvinnie <tvinnie77@gmail.com>
2021-06-29 15:20:35 +0300

    'Personalsite'

---
+++ src/data.js
@@ -0,0 +1,78 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-1.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://reactbootcamp.com",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Mr Kipruto is goal oriented, a team player and determined young man. He demonstrated ingenuity in software development. He was well organized and thrived under adversity. In many instances we placed him in situations that we felt only he could succeed, and he never let us down. Without exception, he put the customer first; felling as we do, that the key to  good business is customer satisfaction.",
+      image: "https://raw.githubusercontent.com/tvinnie/master/master/img/mikeutaf.jpg",
+      name: "Dr. Michael Jairo",
+      company: "Senior Consultant at Utafiti International",
+    },
+    {
+      quote:
+        "Having worked with Too Vincent and assessed output, i fully testify he is good in the following areas of programming: back end developer of Java and PHP, User interface development, MySQL, Javascript and server side processing. I do recommend him in any future consideration.",
+      image: "https://raw.githubusercontent.com/tvinnie/master/master/img/kims.jpg",
+      name: "Kimatia Joshua Daniel",
+      company: "Software Developer",
+    },
+    {
+      quote:
+        "Vincent is a problem solver, he provided adept technical support to colleagues and the organization, he is also very good at java script. I highly recommend him for any tech-related task.",
+      image: "https://raw.githubusercontent.com/tvinnie/master/master/img/lily.png",
+      name: "Lilian Nyambura",
+      company: "Kenafric Industries Ltd",
+    },
+    {
+      quote:
+        "A great guy, with a brilliant mind. All things UI/UX expect him to give you something unique.",
+      image: "https://raw.githubusercontent.com/tvinnie/master/master/img/kevin.jpg",
+      name: "Kevin Tum",
+      company: "Peek and Dale Solutions Ltd",
+    },
+    
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "Next.js",
+    "Node",
+    "GraphQL",
+    "Material UI",
+    "Android",
+    "Java"
+  ];
+  
